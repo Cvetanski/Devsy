e1b44259c8c2ab48d081afd57043fbca458848c8
Cvetanski <dare-cvetanoski@hotmail.com>
2022-01-07 15:03:57 +0100

    CheckIsEmpty

--- app/Http/Controllers/CoffeeBreakPreferenceController.php
+++ app/Http/Controllers/CoffeeBreakPreferenceController.php
@@ -12,18 +12,12 @@ class CoffeeBreakPreferenceController extends Controller
     public function index(CoffeeBreakPreferenceRepositoryInterface $coffeeBreakPreferenceRepository)
     {
         try {
-            if(empty($coffeeBreakPreferenceRepository->all()))
-            return response()->json([
-                'error'=>false,
-                'message'=>'There are no preferences for today',
-                'list'=>'empty'
-            ]);else{
-                return response()->json([
-                    'error'=>false,
-                    'message'=>'Here is the all Preferences for today Coffee Break',
-                    'list'=>$coffeeBreakPreferenceRepository->all()
-                ]);
-            }
+            $items = $coffeeBreakPreferenceRepository->all();
+
+            return response()->json(empty($items)?
+                ['error'=>false,
+                'There Are No Preferences For Today']:['items'=>$items]);
+
         }catch (\Exception $e){
             return response()->json([
                 'error'=>true,
@@ -44,12 +38,11 @@ class CoffeeBreakPreferenceController extends Controller
     public function showXmlPreferences(CoffeeBreakPreferenceRepositoryInterface $coffeeBreakPreferenceRepository)
     {
         try{
-            if (empty($coffeeBreakPreferenceRepository->all())){
-                return response()->xml('There are no preferences for today');
-            }
-            else{
-                return response()->xml(['coffeeBreakPreferenceRepository' => $coffeeBreakPreferenceRepository->all()]);
-            }
+            $items = $coffeeBreakPreferenceRepository->all();
+
+                return response()->xml(empty($items)?
+                'There Are No Preferences For Today':['items'=>$items]);
+
         }catch (\Exception $e){
             return response($e->getMessage());
         }
